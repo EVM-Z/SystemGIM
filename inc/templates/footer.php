703880edfafcabd69882b363b48eff92afa32a1a
EliseoVM <elyzeo.vm@gmail.com>
2020-01-20 14:54:37 -0600

    Arreglando el footer section bg-dark

--- inc/templates/footer.php
+++ inc/templates/footer.php
@@ -1,3 +1,54 @@
+<footer class="footer-section bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="text-white">Acerca de ALFA</h3>
+                <p>ALFA es un gestor de gimnasio para un mejor control y manejo de los clientes.</p>
+            </div>
+
+            <div class="col-md-3 ml-auto">
+                <h3 class="text-white">Links</h3>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Clases</a></li>
+                    <li><a href="#">Schedule</a></li>
+                    <li><a href="#">Trainer</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4">
+                <h3 class="text-white">Subscribete</h3>
+                <p>Actualizaciones, mejoras, noticias y mas. Solo ingresa tu email para ser el primero en enterarte.
+                </p>
+                <form action="#">
+                    <div class="d-flex mb-5">
+                        <input type="text" class="form-control rounded-0" placeholder="Email">
+                        <input type="submit" class="btn btn-primary rounded-0" value="Subscribete">
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <div class=" pt-5">
+                    <p>
+                        Copyright &copy;
+                        <script data-cfasync="false" src="/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+                        <script>
+                            document.write(new Date().getFullYear());
+                        </script> All rights reserved | Todo nuestro trabajo esta hecho con
+                        <i class="icon-heart text-danger" aria-hidden="true"></i>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
+
 <script src="js/jquery-3.3.1.min.js"></script>
 <script src="js/jquery-migrate-3.0.1.min.js"></script>
 <script src="js/jquery-ui.js"></script>
