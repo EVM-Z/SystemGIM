eed7fe0f2e1009c9ccbc35b4dc9374cd7f1846e5
EliseoVM <elyzeo.vm@gmail.com>
2020-10-26 22:02:23 -0600

    Templates | barra-navegacion | footer | header

---
+++ includes/templates/footer.php
@@ -0,0 +1,63 @@
+<footer class="footer-section bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <h3 class="text-white">Acerca de ALFA</h3>
+                <p>ALFA es un gestor de gimnasio para un mejor control y manejo de los clientes.</p>
+            </div>
+
+            <div class="col-md-3 ml-auto">
+                <h3 class="text-white">Links</h3>
+                <ul class="list-unstyled footer-links">
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Clases</a></li>
+                    <li><a href="#">Schedule</a></li>
+                    <li><a href="#">Trainer</a></li>
+                </ul>
+            </div>
+
+            <div class="col-md-4">
+                <h3 class="text-white">Subscribete</h3>
+                <p>Actualizaciones, mejoras, noticias y mas. Solo ingresa tu email para ser el primero en enterarte.
+                </p>
+                <form action="#">
+                    <div class="d-flex mb-5">
+                        <input type="text" class="form-control rounded-0" placeholder="Email">
+                        <input type="submit" class="btn btn-primary rounded-0" value="Subscribete">
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+        <div class="row pt-5 mt-5 text-center">
+            <div class="col-md-12">
+                <div class=" pt-5">
+                    <p>
+                        Copyright &copy; 2020
+                        </script> All rights reserved | Todo nuestro trabajo esta hecho con
+                        <i class="icon-heart text-danger" aria-hidden="true"></i>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
+
+
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/jquery-migrate-3.0.1.min.js"></script>
+<script src="js/jquery-ui.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.stellar.min.js"></script>
+<script src="js/jquery.countdown.min.js"></script>
+<script src="js/bootstrap-datepicker.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="js/aos.js"></script>
+<script src="js/jquery.fancybox.min.js"></script>
+<script src="js/jquery.sticky.js"></script>
+<script src="js/jquery.mb.YTPlayer.min.js"></script>
+<script src="js/main.js"></script>
