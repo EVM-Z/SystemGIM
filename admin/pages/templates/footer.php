7e76a1c975f5cc967fe121b7c4ba30f9c24ffbe8
EliseoVM <elyzeo.vm@gmail.com>
2020-10-27 22:56:21 -0600

    Agregando AdminLTE

---
+++ admin/pages/templates/footer.php
@@ -0,0 +1,40 @@
+
+  <footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      <b>Version</b> 3.0.5
+    </div>
+    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> All rights
+    reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+
+<!-- jQuery -->
+<script src="../plugins/jquery/jquery.min.js"></script>
+<!-- SweetAlert -->
+<script src="../dist/js/sweetalert2.all.min.js"></script>
+<!-- Ajax -->
+<!-- <script src="../dist/js/admin-ajax.js"></script> -->
+<!-- Login -->
+<!-- <script src="../dist/js/login-ajax.js"></script> -->
+<!-- Bootstrap 4 -->
+<script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="../plugins/datatables/jquery.dataTables.js"></script>
+<script src="../plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script>
+<!-- Formato en Español para el DataTables -->
+<!-- <script src="../dist/js/app.js"></script> -->
+<!-- AdminLTE App -->
+<script src="../dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../dist/js/demo.js"></script>
+
+</body>
+</html>
