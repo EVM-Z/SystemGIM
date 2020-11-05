1c5642c962601d624e3d8ce282a7b0c015f55917
EliseoVM <elyzeo.vm@gmail.com>
2020-11-05 15:21:20 -0600

    Agregando app.js para el DataTables Español

---
+++ admin/dist/js/app.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    // El footer/paginación de DataTables en Español
+    $("#registros").DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": true,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        'language': {
+            paginate: {
+                next: 'Siguiente',
+                previous: 'Anterior',
+                last: 'Último',
+                first: 'Primero'
+            },
+            info: 'Mostrando _START_ a _END_ de _TOTAL_ resultados',
+            emptyTable: 'No hay registros',
+            infoEmpty: '0 Registros',
+            search: 'Buscar: '
+        }
+    });
+
+});
